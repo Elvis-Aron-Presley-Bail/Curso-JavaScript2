cfdae3a97a324aabbf939e380568e9a39efc06f7
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-06-09 12:11:23 -0300

    Calcular média

---
+++ 2-CalcularMedia/calcularMedia.js
@@ -0,0 +1,9 @@
+let nota1 = 9
+let nota2 = 8
+let media = (nota1 + nota2) / 2
+
+if (media >= 7) {
+    console.log(`Aprovado com média ${media}`)
+} else {
+    console.log(`Reprovado com média ${media}`)
+}
