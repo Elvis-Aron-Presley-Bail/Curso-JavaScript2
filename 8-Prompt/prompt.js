c881dfd1dc7e412e4e2ed9a9ea074080af8970f3
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-06-09 12:12:24 -0300

    Prompt

---
+++ 8-Prompt/prompt.js
@@ -0,0 +1,12 @@
+let alunos = []
+
+let numAlunos = parseInt(prompt("Quantos alunos voce quer cadastrar?"))
+
+for(let i = 0; i < numAlunos; i++) {
+    let nome = prompt(`Digite o nome do aluno ${i + 1}: `)
+    let idade = parseInt(prompt(`Digite a idade do aluno ${i + 1}: `))
+    let nota1 = parseInt(prompt(`Digite a nota ${i + 1}: `))
+    let nota2 = parseInt(prompt(`Digite a nota ${i + 1}: `))
+    let nota3 = parseInt(prompt(`Digite a nota ${i + 1}: `))
+    let nota4 = parseInt(prompt(`Digite a nota ${i + 1}: `))
+}
