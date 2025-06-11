8d01672c2e456cf3ba2d04028db08f2d5408d3f8
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-06-11 11:22:07 -0300

    Prompt

--- 8-Prompt/prompt.js
+++ 8-Prompt/prompt.js
@@ -1,12 +1,37 @@
 let alunos = []
+let quantidade = parseInt(prompt('Quantos alunos vao se cadastrar? '))
 
-let numAlunos = parseInt(prompt("Quantos alunos voce quer cadastrar?"))
+for (let i = 0; i < quantidade; i++) {
+    let nome = prompt(`Digite o nome do aluno ${i+1}: `)
+    let idade = parseInt(prompt(`Digite a idade do aluno ${i+1}: `))
 
-for(let i = 0; i < numAlunos; i++) {
-    let nome = prompt(`Digite o nome do aluno ${i + 1}: `)
-    let idade = parseInt(prompt(`Digite a idade do aluno ${i + 1}: `))
-    let nota1 = parseInt(prompt(`Digite a nota ${i + 1}: `))
-    let nota2 = parseInt(prompt(`Digite a nota ${i + 1}: `))
-    let nota3 = parseInt(prompt(`Digite a nota ${i + 1}: `))
-    let nota4 = parseInt(prompt(`Digite a nota ${i + 1}: `))
+    let notas = []
+    for (let j = 0; j < 4; j++) {
+        let nota = parseInt(prompt(`Digite a nota ${j+1}`))
+        notas.push(nota)
+    }
+
+    let aluno = {
+        nome: nome,
+        idade: idade,
+        notas: notas
+    }
+
+    alunos.push(aluno)
+}
+
+function calcularMedia(notas) {
+    let soma = 0
+    for (let i = 0; i < notas.length; i++) {
+        soma += notas[i]
+    }
+    return soma / notas.length
+}
+
+function verificarSituacao(media) {
+    return media >= 7 ? "Aprovado" : "Reprovado"
+}
+
+for (let i = 0; i < alunos.length; i++) {
+    
 }
